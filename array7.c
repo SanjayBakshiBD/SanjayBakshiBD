3a5d7eaadda8ff02af7b590dedb0eabd2f663eae
Sanjay Bakshi <sanjaybakshi647@gmail.com>
2021-04-22 22:00:10 +0600

    Added array7.c file

---
+++ array7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    int b=1;
+    int c=1;
+
+    while(b<=a)
+    {
+        c=1;
+        while(c<=a)
+        {
+            printf("%d ", c);
+            c=c+1;
+        }
+        b=b+1;
+        printf("\n");
+    }
+    return 0;
+}
